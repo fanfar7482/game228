b5cbd9e53efddd1eeddd79721ca2806a0ba57d52
FANFAR <danikmax2005@gmail.com>
2026-01-30 18:08:31 +0100

    ralib

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <source/raylib/raylib.h>
-#include <source/raylib/raymath.h>
+#include <source/raylib/5.5/include/raylib.h>
+#include <source/raylib/5.5/include/raymath.h>
 #include <random>
 #include <chrono>
 #include <thread>
+
