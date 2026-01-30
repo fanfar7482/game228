f0ecd37141ac4b51a7d024a828490b3b3c8de9ce
FANFAR <danikmax2005@gmail.com>
2026-01-30 19:45:45 +0100

    start

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
-#include <source/raylib/5.5/include/raylib.h>
-#include <source/raylib/5.5/include/raymath.h>
+#include "raylib/5.5/include/raylib.h"
+#include "raylib/5.5/include/raymath.h"
 #include <random>
 #include <chrono>
 #include <thread>
 
+void drawBoard() {
+    for (int i = 0; i <= 54 i++) {
+        for (int j = 0; j)
+    }
+}
+
+int main() {
+
+    int windowX = 1920, windowY = 1080;
+    
+    InitWindow(windowX, windowY, "qwerty");
+    
+    while(!WindowShouldClose()) {
+        
+        BeginDrawing();
+
+
+        EndDrawing();
+    }
+
+    return 0;
+}
