8cf5d3183fc4b97d71a35758fdb1662ee82c76e3
FANFAR <danikmax2005@gmail.com>
2026-02-02 19:55:05 +0100

    setCellColor OK, DrawBoard OK, initBoard OK

--- source/main.cpp
+++ source/main.cpp
@@ -5,19 +5,48 @@
 #include <chrono>
 #include <thread>
 
-void drawBoard(int rows, int cols, float cellSize) {
+struct Cell {
+    Rectangle rec;
+    Color color;
+};
 
-    Rectangle r1 = {0, 0, cellSize, cellSize};
+std::vector<std::vector<Cell>> board;
+int BOARD_ROWS, BOARD_COLS;
+
+void initBoard(int rows, int cols, float cellSize) {
+    BOARD_ROWS = rows;
+    BOARD_COLS = cols;
+
+    board.clear();
+    board.resize(rows, std::vector<Cell>(cols));
 
     for (int row = 0; row < rows; row++) {
         for (int col = 0; col < cols; col++) {
-            r1.x = col * cellSize;
-            r1.y = row * cellSize;
-
-            DrawRectangleRec(r1, GREEN);
-            DrawRectangleLinesEx(r1, (cellSize / 80), GRAY);
+            board[row][col].rec = {col * cellSize, row * cellSize, cellSize, cellSize};
+            board[row][col].color = DARKGRAY;
         }
     }
+}
+
+void setCellColor(int row, int col, Color color) {
+    if (row < 0 || row >= BOARD_ROWS) return;
+    if (col < 0 || col >= BOARD_COLS) return;
+
+    board[row][col].color = color;
+}
+
+void drawBoard() {
+    for (int row = 0; row < BOARD_ROWS; row++) {
+        for (int col = 0; col < BOARD_COLS; col++) {
+            const Cell& cell = board[row][col];
+            DrawRectangleRec(cell.rec, cell.color);
+            DrawRectangleLines(cell.rec.x, cell.rec.y, cell.rec.width, cell.rec.height, PINK);
+        }
+    }
+}
+
+void start() {
+
 }
 
 int main() {
@@ -25,12 +54,14 @@ int main() {
     int windowX = 1920, windowY = 1080;
     
     InitWindow(windowX, windowY, "qwerty");
+    initBoard(24, 52, 30);
+    setCellColor(12, 26, RED);
     
     while(!WindowShouldClose()) {
         
         BeginDrawing();
 
-        drawBoard(24, 54, 30);
+        drawBoard();
 
         EndDrawing();
     }
